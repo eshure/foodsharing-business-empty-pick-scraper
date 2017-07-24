591e70c3d10d2a31c0b3962bd8f7115bc2029286
karsten sammann hiekmann <sammann@fnordbedarf.de>
2017-07-24 17:13:42 +0200

    add gradle and smoke test google

---
+++ src/test/java/smoke/GetGoogle.java
@@ -0,0 +1,11 @@
+package smoke;
+
+import org.junit.Test;
+
+public class GetGoogle {
+
+    @Test
+    public void given_internet_when_accessing_google_then_expect_page_title() {
+        assert(true);
+    }
+}
